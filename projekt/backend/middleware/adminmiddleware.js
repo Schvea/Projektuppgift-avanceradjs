58e84841a3e35ba2ef6344d2c102122ac6bff863
scvhea <svea.holm03@gmail.com>
2025-10-10 20:28:20 +0200

    middleware admin

--- projekt/backend/middleware/adminmiddleware.js
+++ projekt/backend/middleware/adminmiddleware.js
@@ -1,1 +1,8 @@
-function checkAdmin(req, res, next) 
+function checkAdmin(req, res, next) {
+  if (req.user.role !== 'admin') {
+    return res.status(403).json({ error: 'Endast admin har åtkomst' });
+  }
+  next();
+}
+
+module.exports = checkAdmin;
