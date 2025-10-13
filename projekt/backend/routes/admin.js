849a87588eaa3b03f3d35b9217901cbe0b73422f
scvhea <svea.holm03@gmail.com>
2025-10-13 17:06:06 +0200

    delete fungerar i insomia

--- projekt/backend/routes/admin.js
+++ projekt/backend/routes/admin.js
@@ -13,5 +13,13 @@ router.get('/users', authenticate, checkAdmin, async (req, res) => {
     res.status(500).json({ error: 'Kan ej hämta användare' });
   }
 });
+router.delete('/users/:id', authenticate, checkAdmin, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Användare raderad' });
+  } catch (err) {
+    res.status(500).json({ error: 'Kunde inte radera användare' });
+  }
+});
 
 module.exports = router;
