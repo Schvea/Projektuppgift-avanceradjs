9d134256a89ede98405a008417a1c94e0cc3d53b
scvhea <svea.holm03@gmail.com>
2025-10-15 16:22:18 +0200

    hämta alla uppgifter fungerar

--- projekt/backend/routes/tasks.js
+++ projekt/backend/routes/tasks.js
@@ -4,4 +4,32 @@ const authenticate = require('../middleware/authmiddleware');
 
 const router = express.Router();
 
+router.post('/tasks', authenticate, async (req, res) => {
+  try {
+    const { title, description, date } = req.body;
+
+    if (!title) {
+      return res.status(400).json({ error: 'Skriv titel' });
+    }
+    const newTask = new Task({
+      title,
+      description,
+      date
+    });
+    const savedTask = await newTask.save();
+    res.status(201).json(savedTask);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'du gör fel' });
+  }
+});
+router.get('/tasks', authenticate, async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Kunde inte hämta sysslor' });
+  }
+});
 module.exports = router;
