afda99c5b700c976de5330978eaeafab9075e1a9
scvhea <svea.holm03@gmail.com>
2025-10-18 21:13:19 +0200

    jwt måsye med

--- projekt/frontend/src/routes/UserRoute.jsx
+++ projekt/frontend/src/routes/UserRoute.jsx
@@ -2,12 +2,14 @@ import { Route } from 'react-router-dom';
 import PanelPage from '../pages/PanelPage';
 import TaskList from '../pages/TaskList';
 import UserManagement from '../pages/UserManagement';
+import CreateTaskPage from '../pages/CreateTaskPage';
 
 export const userRoutes = (
   <>
     <Route path="/panel" element={<PanelPage />} />
     <Route path="/tasks" element={<TaskList />} />
     <Route path="/users" element={<UserManagement />} />
+    <Route path="/tasks/create" element={<CreateTaskPage />} />
   </>
 );
 
