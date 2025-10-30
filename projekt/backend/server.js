3c99022a4484e5d5035cf82884951d33b7053aea
scvhea <svea.holm03@gmail.com>
2025-10-30 15:29:50 +0100

    frontend represeantion render

--- projekt/backend/server.js
+++ projekt/backend/server.js
@@ -33,6 +33,12 @@ const taskRoute = require('./routes/tasks');
 
 app.use('/api', taskRoute);
 
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+});
+
 mongoose
   .connect(process.env.MONGO_URI, {})
   .then(() => {
